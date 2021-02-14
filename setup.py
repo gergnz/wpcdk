e0998ab16fe85671a0003a3358077b0575a8e9c2
Greg Cockburn <gergnz@gmail.com>
2021-02-14 20:49:02 +1100

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     version="0.0.1",
 
     description="Basic Wordpress Setup",
-    long_description=description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
 
     author="gergnz",
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="wpcdk"),
 
     install_requires=[
-        "aws-cdk.core====1.74.0",
+        "aws-cdk.core==1.74.0",
         "aws-cdk.aws-ec2==1.74.0",
         "aws-cdk.aws-rds==1.74.0",
         "aws-cdk.aws-iam==1.74.0",
